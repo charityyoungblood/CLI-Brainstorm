2e639dfd7e20e461b36551ecdd5674862bc7d227
charityyoungblood <youngblood.charity@gmail.com>
2018-01-04 17:27:09 -0800

    added comments for type of CLI to create

---
+++ outline.rb
@@ -0,0 +1,12 @@
+How to Build a CLI Gem - Project Outline
+
+1. Plan your gem, imagine the interface the user is going to use while interacting with your gem  
+  - A command line interface for Aquazzura shoes, where user can sort by color, heel height or shoe type
+  
+2. Start with the project structure - Google
+3. Start with the entry point - the file run
+4. Force that to build the CLI interface
+5. Stub out the interface
+6. Start making things real 
+7. Discover objects 
+8. Program 
