d7ddf62c1b7382b8176b0456054c74d3ab24726a
charityyoungblood <youngblood.charity@gmail.com>
2018-01-04 17:30:58 -0800

    added user prompts - what user will see

--- outline.rb
+++ outline.rb
@@ -2,7 +2,15 @@ How to Build a CLI Gem - Project Outline
 
 1. Plan your gem, imagine the interface the user is going to use while interacting with your gem  
   - A command line interface for Aquazzura shoes, where user can sort by color, heel height or shoe type
-  
+  - prompt the user with a greeting
+  - What type of occassion are you shopping for?
+  - What color shoes?
+  - What heel height? 
+    
+  Ex. display 
+    
+    Black, heel shoe with bow $750 
+    
 2. Start with the project structure - Google
 3. Start with the entry point - the file run
 4. Force that to build the CLI interface
